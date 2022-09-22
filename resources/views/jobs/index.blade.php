ce23082cb968552f224a01652394c520f5dfca77
Butrint Islami <butrint-i@hotmail.com>
2022-09-22 15:30:36 +0200

    deri ktu mir bablok

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,11 @@
+<x-layouts>
+    @include('Partials._hero')
+    @include('Partials._search')
+
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @foreach($jobs as $job)
+            <x-jobs-card :job="$job"></x-jobs-card>
+        @endforeach
+    </div>
+
+</x-layouts>
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\JobController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Models\Jobs;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[JobController::class, 'index']);
+
+
+//veq ni pun
+Route::get('/puna/{job}', [JobController::class, 'show']);
+
+
+
+
+Route::get('/layouts', function () {
+    return view('layouts');
+});
+
+Route::resource('/jobs',JobController::class);
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+////Single listings
+//Route::get('/puna/{id}', function($id){
+//    return view('puna',[
+//        'puna'=>jobs::find($id)
+//    ]);
+//});
